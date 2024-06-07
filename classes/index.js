7596da1944cd981039e453f4f023d801a855db00
u4ik <insighteuphoric@gmail.com>
2024-06-06 20:42:10 -0400

    added classes notes

--- classes/index.js
+++ classes/index.js
@@ -1,4 +1,4 @@
-//? Classes 
+//? Classes
 /* 
     - Introduced in 2015
     - A function that helps us create objects
@@ -17,23 +17,23 @@ class NameOfClass {
 */
 
 class NewObject {
-    static name = "name default"
-    desc = "desc default"
+  static name = "name default";
+  desc = "desc default";
 }
-let oneObj = new NewObject()
-oneObj.desc = "Something else"
+let oneObj = new NewObject();
+oneObj.desc = "Something else";
 console.log(oneObj);
 
 // 1    2
 class Item {
-    //  3               4
-    constructor(name, desc, price) {
-      //5    6      7
-      this.name = name;
-      this.description = desc;
-      this.price = price;
-      // this.defaultKey = "Default";
-    }
+  //  3               4
+  constructor(name, desc, price) {
+    //5    6      7
+    this.name = name;
+    this.description = desc;
+    this.price = price;
+    // this.defaultKey = "Default";
+  }
 }
 
 /* 
@@ -45,3 +45,100 @@ class Item {
     6. The name of the key to our new object.
     7. The value for those keys being created (set by parameters).
 */
+
+// let itemZero = new Item();
+// console.log(itemZero);
+
+let itemOne = new Item("Beans", "Canned", 0.89);
+
+// let brokenItem = Item() // !TypeError: Class constructor Item cannot be invoked without 'new'
+// console.log(brokenItem);
+
+let iType = "Tomato Soup";
+let iDesc = "Canned";
+let iCost = 1.29;
+
+function processItem(i, d, c) {
+    //... 
+  return new Item(i, d, c);
+}
+
+let useFunction = processItem(iType, iDesc, iCost);
+console.log(useFunction);
+
+class DeptInventory {
+    constructor(dept){
+        this.department = dept
+        this.items = []
+    }
+
+    addToInventory (newItem) {
+        this.items.push(newItem)
+        console.log("Item was added to the inventory!");
+    }
+}
+
+let dryGoods = new DeptInventory("Dry Goods")
+
+let itemThree = new Item("Dog Food", "Bag", 4.50 )
+
+dryGoods.addToInventory(itemThree)
+
+console.log(dryGoods);
+
+//? Inheritance
+// Parent Class
+class Car {
+    constructor(brand){
+        this.brandName = brand
+    }
+    present(){
+        return `I have a ${this.brandName}`
+    }
+}
+
+// Child Class
+class Model extends Car {
+    constructor(brandName, model){
+        super(brandName);
+        this.model = model
+    }
+
+    show(){
+        return `${this.present()} and it is a ${this.model} `
+    }
+}
+
+let myCar2 = new Model("Subaru", "STI")
+let myCar3 = new Model("Tesla", "Cybertruck")
+
+console.log(myCar3.show())
+
+// let myCar1 = new Car("Subaru")
+
+// console.log(myCar1.present());
+
+
+class Expense {
+    static addUpchargeForProfit(wholesale, upchargePercent){
+        let upcharge = wholesale*upchargePercent + wholesale
+        return new Expense(wholesale, upcharge)
+    }
+
+    constructor(wholesale, sellAt){
+        this.purchased_price = wholesale
+        this.sell_at = sellAt
+        this.sales_tax
+    }
+
+    addTax(x){
+        let percentage = x;
+        let sellingAtCost = this.sell_at
+        this.sales_tax = parseFloat((sellingAtCost + (sellingAtCost*percentage)).toFixed(2))
+    }
+}
+
+let itemToSell = Expense.addUpchargeForProfit(5,.10)
+itemToSell.addTax(0.07)
+
+console.log(itemToSell)
